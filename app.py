cabe2cc7ab25aa516edca709238ba0c303aa0535
Aakriti-malla <aakritimalla91@gmail.com>
2023-08-24 16:51:07 +0530

    Refactoring of Auth

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy
 from hashlib import sha256
 from flask_mysqldb import MySQL
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import secrets
 
 app = Flask(__name__)
+
+secret_key = secrets.token_hex(32)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'admin'
 app.config['MYSQL_DB'] = 'rev_pay'
+app.config['JWT_SECRET_KEY'] = secret_key
 
 mysql = MySQL(app)
+jwt = JWTManager(app)
 
 
 # Route for Registration
@@ -18,13 +23,14 @@ mysql = MySQL(app)
 def register():
     data = request.json
 
-    if 'username' not in data or 'password' not in data or 'company_name' not in data:
-        return jsonify({"error": "Company name, Username & passwordare required"}), 400
+    required_fields = ['username','password', 'company_name']
+
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Company name, username, and password are required"}), 400
 
     company_name = data['company_name']
     username = data['username']
     password = data['password']
-
     password_hash = sha256(password.encode()).hexdigest()
 
     cur = mysql.connection.cursor()
@@ -48,36 +54,39 @@ def login():
     data = request.json
     username = data['username']
     password = data['password']
-
     password_hash = sha256(password.encode()).hexdigest()
+    
     cur = mysql.connection.cursor()
-    cur.execute("SELECT company_id FROM Businesses WHERE username = %s AND password = %s", (username, password_hash))
+    cur.execute("SELECT company_id FROM businesses WHERE username = %s AND password = %s", (username, password_hash))
     result = cur.fetchone()
     
     if result:
-        return jsonify({"message": "Login successful", "company_id": result[0]})
+        access_token = create_access_token(identity=result[0])
+        return jsonify({"message": "Login successful", "access_token": access_token})
     else:
         return jsonify({"message": "Invalid credentials"}), 401
 
 # Route to create a bank account
 @app.route('/create_account', methods=['POST'])
+@jwt_required()
 def create_account():
+    current_user = get_jwt_identity()
     data = request.json
-    company_id = data['company_id']
+    # company_id = data['company_id']
+    required_fields = ['bank_account_number', 'ifsc_code']
+
+    if not all(field in data for field in required_fields):
+        return jsonify({"error": "Bank account number and IFSC code are required"}), 400
+    
     bank_account_number = data['bank_account_number']
     ifsc_code = data['ifsc_code']
 
     cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO BankAccounts (company_id, bank_account_number, ifsc_code) VALUES (%s, %s, %s)", (company_id, bank_account_number, ifsc_code))
+    cur.execute("INSERT INTO bankaccounts (company_id, bank_account_number, ifsc_code) VALUES (%s, %s, %s)", (current_user, bank_account_number, ifsc_code))
     mysql.connection.commit()
     cur.close()
     
     return jsonify({"message": "Bank account created successfully"})    
-
-
-@app.route('/')
-def hello():
-    return "Welcome to Rev Pay"
 
 if __name__ == '__main__':
     app.run()
